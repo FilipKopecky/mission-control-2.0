196cb62129ad9cae25427f08d15ca251caca5062
Filip Kopecký <kopeckyfil@gmail.com>
2022-10-12 23:42:45 +0200

    Added icons + basic output of fetched data

--- src/api/VocabularyApi.ts
+++ src/api/VocabularyApi.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { Vocabulary } from "../@types";
+import { BaseVocabularyData } from "../@types";
 import { getVocabulariesUrl } from "./endpoints";
 import Ajax from "../utils/Ajax";
 
-const getVocabularies = (): Promise<Vocabulary[]> =>
+const getVocabularies = (): Promise<BaseVocabularyData[]> =>
   Ajax.get(getVocabulariesUrl()).then((resp) => resp.data);
 
 export const useVocabularies = () => {
--- src/components/Vocabularies.tsx
+++ src/components/Vocabularies.tsx
@@ -4,10 +4,14 @@ import { useVocabularies } from "../api/VocabularyApi";
 const Vocabularies: React.FC = () => {
   const { data = [], isLoading } = useVocabularies();
   if (isLoading) return <h2>Loading...</h2>;
-  console.log(data);
   return (
     <div>
       <h2>Vocabularies</h2>
+      <ul>
+        {data.map((vocabulary) => (
+          <li>{vocabulary.label}</li>
+        ))}
+      </ul>
     </div>
   );
 };
